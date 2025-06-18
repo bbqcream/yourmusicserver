920292eb93778ff4b25dc30b54b8978bfa87a19f
bbqcream <imground11@gmail.com>
2025-06-18 09:59:15 +0900

    init

---
+++ src/test/kotlin/com/yourmusic/yourmus1cserver/Yourmus1CServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yourmusic.yourmus1cserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Yourmus1CServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
